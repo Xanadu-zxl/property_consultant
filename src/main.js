52e8a54db442eca1b83b2f34b37ebde5361aaceb
Xanadu-zxl <xanadu.zxl@gmail.com>
2020-04-28 21:30:46 +0800

    feat：按需映入UI库vant

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import { Tabbar, TabbarItem } from 'vant'
+
+Vue.use(Tabbar)
+Vue.use(TabbarItem)
 
 Vue.config.productionTip = false
 
