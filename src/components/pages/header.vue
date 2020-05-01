d15542e0851bbed7423d41636e0388b62e52807f
Xanadu-zxl <xanadu.zxl@gmail.com>
2020-05-01 17:28:25 +0800

    feat：header组件样式修改

--- src/components/pages/header.vue
+++ src/components/pages/header.vue
@@ -1,8 +1,14 @@
 <template>
   <div>
     <header class="header">
-      <div class="left">新客户</div>
-      <div class="right">工具箱</div>
+      <div class="box">
+        <p class="title">新客户</p>
+        <img src="@/assets/img/Index-Card-Users.png" class="img">
+      </div>
+      <div class="box">
+        <p class="title">工具箱</p>
+        <img src="@/assets/img/Index-Card-Tool.png" class="img">
+      </div>
     </header>
   </div>
 </template>
@@ -18,6 +24,31 @@ export default {
 }
 </script>
 
-<style>
+<style  scoped>
+.header {
+  width: 88%;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-around;
+}
+.box {
+  width: 47%;
+  position: relative;
+  border-radius: 0.3125rem;
+}
 
+.title {
+  color: #fff;
+  position: absolute;
+  left: 16px;
+  top: 16px;
+  font-size: 18px;
+  font-weight: 600;
+}
+
+.img{
+  width:100%;
+  border-radius: 0.3125rem;
+
+}
 </style>
