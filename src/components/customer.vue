87cc475f10bf59f50981350b0e02de6b64916d56
Xanadu-zxl <xanadu.zxl@gmail.com>
2020-05-03 22:54:42 +0800

    feat：customer开发

---
+++ src/components/customer.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <customer-tabbar :title="title" />
+  </div>
+</template>
+
+<script>
+import CustomerTabbar from './pages/tabbar'
+export default {
+  data () {
+    return {
+      title: '新客户'
+
+    }
+  },
+  components: {
+    CustomerTabbar
+  }
+
+}
+</script>
+
+<style  scoped>
+
+</style>
---
+++ src/components/pages/tabbar.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <header class="tabnar-header">
+      <i class="icon-Index-Icon-Down"></i>
+      {{title}}
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['title'],
+  data () {
+    return {
+
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+.tabnar-header {
+  text-align: center;
+  position: relative;
+  font-size: 16px;
+  font-weight: 600;
+  color: #222;
+  height: 50px;
+  line-height: 50px;
+}
+.icon-Index-Icon-Down {
+  position: absolute;
+  left: 10px;
+}
+</style>
