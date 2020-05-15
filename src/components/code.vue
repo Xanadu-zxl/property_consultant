f61f39ff610e298d9423b14980e70544194b1d28
Xanadu-zxl <xanadu.zxl@gmail.com>
2020-05-15 23:44:25 +0800

    feat；权限校验

--- src/components/code.vue
+++ src/components/code.vue
@@ -7,53 +7,60 @@
 </template>
 
 <script>
-// import qs from 'qs'
 export default {
   data () {
     return {
       code: '',
       token: '',
-      id: '',
       // id: '73',
-      phone: '18980807092',
-      // phone: '',
-      name: ''
+      // phone: '18980807092',
+      id: '',
+      phone: '',
+      name: '',
+      client_id: '',
+      client_secret: ''
     }
   },
   mounted () {
-    console.log(this.$route.query.code)
     this.code = this.$route.query.code
-    console.log(this.code)
-
-    this.$cookies.set('CURRENT-USER-ID', this.id)
-    this.$cookies.set('CURRENT-USER-PHONE', this.phone)
-    this.$cookies.set('CURRENT-NAME', this.name)
+    // this.$cookies.set('CURRENT-USER-ID', this.id)
+    // this.$cookies.set('CURRENT-USER-PHONE', this.phone)
+    // this.$cookies.set('CURRENT-NAME', this.name)
     this.$axios({
-      method: 'POST',
-      url: '/oauth/token',
-      headers: { 'Content-Type': 'application/json' },
-      params: {
-        client_id: 'e84c788580a5bdc0353b680f6c4d6ef4b10894b220f544d3dd97f27366a02b8c',
-        client_secret: '4920df80ee57d9c160bd8c017a014cccf539625434d775fc81410c891768ef9a',
-        code: this.code,
-        grant_type: 'authorization_code',
-        redirect_uri: 'http://localhost:8080/real_estate/saler/code'
-      }
+      method: 'get',
+      url: '/magnate/oauth'
     }).then((res) => {
-      console.log(res)
-      this.token = res.data.access_token
-      console.log(res.data.access_token)
+      this.client_id = res.data.client_id
+      this.client_secret = res.data.client_secret
+
       this.$axios({
-        method: 'GET',
-        url: '/api/v1/user?access_token=' + this.token
+        method: 'POST',
+        url: '/oauth/token',
+        headers: { 'Content-Type': 'application/json' },
+        params: {
+          // client_id: 'e84c788580a5bdc0353b680f6c4d6ef4b10894b220f544d3dd97f27366a02b8c',
+          // client_secret: '4920df80ee57d9c160bd8c017a014cccf539625434d775fc81410c891768ef9a',
+          client_id: this.client_id,
+          client_secret: this.client_secret,
+          code: this.code,
+          grant_type: 'authorization_code',
+          redirect_uri: 'http://localhost:8080/real_estate/saler/code'
+        }
       }).then((res) => {
         console.log(res)
-        this.id = res.data.id
-        this.name = res.data.name
-        // this.phone = data.data.phone
-        this.$cookies.set('CURRENT-USER-ID', this.id)
-        this.$cookies.set('CURRENT-USER-PHONE', this.phone)
-        this.$cookies.set('CURRENT-NAME', this.name)
+        this.token = res.data.access_token
+        this.$axios({
+          method: 'GET',
+          url: '/api/v1/user?access_token=' + this.token
+        }).then((res) => {
+          console.log(res)
+          this.id = res.data.id
+          this.name = res.data.name
+          this.phone = res.data.phone
+          this.$cookies.set('CURRENT-USER-ID', res.data.id)
+          this.$cookies.set('CURRENT-USER-PHONE', res.data.phone)
+          this.$cookies.set('CURRENT-NAME', res.data.name)
+        })
       })
     })
 
