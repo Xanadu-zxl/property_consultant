4f4d08854351706dcefcb33799166963b1946e2e
xanadu-zxl <xanadu.zxl@gamil.com>
2020-07-01 23:40:19 +0800

    feat：修改

--- src/components/pages/header.vue
+++ src/components/pages/header.vue
@@ -1,13 +1,9 @@
 <template>
   <div>
     <header class="header">
-      <router-link class="box" to="/real_estate/saler/customer">
-        <p class="title">新客户</p>
-        <img class="img" src="@/assets/img/Index-Card-Users.png" />
-      </router-link>
-      <router-link @click.native="prompt" class="box" to="#">
-        <p class="title">工具箱</p>
-        <img class="img" src="@/assets/img/Index-Card-Tool.png" />
+      <router-link class="box" to="/real_estate/saler/arrive_visitors">
+        <p class="title">新建客户</p>
+        <img class="img" src="@/assets/img/Card-Visit.png" />
       </router-link>
     </header>
   </div>
@@ -31,13 +27,13 @@ export default {
 
 <style  scoped>
 .header {
-  width: 88%;
+  width: 90%;
   margin: 60px auto 0px;
   display: flex;
   justify-content: space-around;
 }
 .box {
-  width: 47%;
+  width: 100%;
   position: relative;
   border-radius: 4;
 }
