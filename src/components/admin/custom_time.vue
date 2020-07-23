2cbaae16e4fc5b71adc0508a2929d038c201760c
xanadu-zxl <xanadu.zxl@gamil.com>
2020-07-23 23:02:28 +0800

    feat；今日详情纬度修改

--- src/components/admin/custom_time.vue
+++ src/components/admin/custom_time.vue
@@ -15,51 +15,66 @@
       </div>
     </header>
     <div class="custom_time_main">
-      <p class="custom_time_content_header">
-        <span>类型</span>
-        <span>新增</span>
-        <span>总占比数</span>
-      </p>
-      <p class="custom_time_content_body">
-        <span>到访客户</span>
-        <span>{{ newArrive }}</span>
-        <span>{{ percentageArrive }}</span>
-      </p>
-      <p class="custom_time_content_body">
-        <span>来电客户</span>
-        <span>{{ newCaller }}</span>
-        <span>{{ percentageCaller }}</span>
-      </p>
-      <p class="custom_time_content_body">
-        <span>银行放款</span>
-        <span>0</span>
-        <span>0%</span>
-      </p>
-      <p class="custom_time_content_body">
-        <span>换房数量</span>
-        <span>0</span>
-        <span>0%</span>
-      </p>
-      <p class="custom_time_content_body">
-        <span>退房数量</span>
-        <span>0</span>
-        <span>0%</span>
-      </p>
-      <p class="custom_time_content_body">
-        <span>更名数量</span>
-        <span>0</span>
-        <span>0%</span>
-      </p>
-      <p class="custom_time_content_body">
-        <span>预约数量</span>
-        <span>0</span>
-        <span>0%</span>
-      </p>
-      <p class="custom_time_content_body">
-        <span>预约已到访人数</span>
-        <span>0</span>
-        <span>0%</span>
-      </p>
+      <van-collapse accordion v-model="activeName">
+        <van-collapse-item value="10 人" icon=" icon-Index-Icon-Foot" name="1" title="到访人数">
+          <p class="custom_content_body">
+            <span>成交人数</span>
+            <span>10 人</span>
+          </p>
+          <p class="custom_content_body">
+            <span>成交率</span>
+            <span>100%</span>
+          </p>
+        </van-collapse-item>
+      </van-collapse>
+
+      <van-collapse accordion v-model="activeName">
+        <van-collapse-item value="10 人" icon=" icon-Index-Icon-Warning" name="2" title="来电人数">
+          <p class="custom_content_body">
+            <span>到访人数</span>
+            <span>10 人</span>
+          </p>
+          <p class="custom_content_body">
+            <span>电转访率</span>
+            <span>100%</span>
+          </p>
+        </van-collapse-item>
+      </van-collapse>
+
+      <van-collapse accordion v-model="activeName">
+        <van-collapse-item value="10 人" icon=" icon-Index-Icon-File" name="3" title="预约人数">
+          <p class="custom_content_body">
+            <span>预约已到访数</span>
+            <span>10 人</span>
+          </p>
+          <p class="custom_content_body">
+            <span>预约到访率</span>
+            <span>100%</span>
+          </p>
+        </van-collapse-item>
+      </van-collapse>
+
+      <van-collapse accordion v-model="activeName">
+        <van-collapse-item value="0 户" icon=" icon-Index-Icon-info" name="4" title="银行放款户数">
+          <p class="custom_content_body">
+            <span>未放款户数</span>
+            <span>0 户</span>
+          </p>
+          <p class="custom_content_body">
+            <span>放款率</span>
+            <span>0 %</span>
+          </p>
+        </van-collapse-item>
+      </van-collapse>
+      <van-collapse accordion v-model="activeName">
+        <van-collapse-item value="0 个" icon=" icon-Index-Icon-Statistics" name="5" title="更名数量"></van-collapse-item>
+      </van-collapse>
+      <van-collapse accordion v-model="activeName">
+        <van-collapse-item value="0 个" icon=" icon-Index-Icon-Statistics" name="6" title="退房数量"></van-collapse-item>
+      </van-collapse>
+      <van-collapse accordion v-model="activeName">
+        <van-collapse-item value="0 个" icon=" icon-Index-Icon-Statistics" name="7" title="换房数量"></van-collapse-item>
+      </van-collapse>
     </div>
   </div>
 </template>
@@ -78,7 +93,8 @@ export default {
       newArrive: 0,
       totalArrive: 1,
       newCaller: 0,
-      totalCaller: 1
+      totalCaller: 1,
+      activeName: "",
     };
   },
   computed: {
@@ -87,7 +103,7 @@ export default {
     },
     percentageCaller() {
       return Math.round((this.newCaller / this.totalCaller) * 100) + "%";
-    }
+    },
   },
   mounted() {},
   methods: {
@@ -100,7 +116,7 @@ export default {
       this.startDate = this.formatDate(start);
       this.endDate = this.formatDate(end);
       let params = { start_date: this.startDate, end_date: this.endDate };
-      api.getAdminCustomDatelineAPI(params).then(res => {
+      api.getAdminCustomDatelineAPI(params).then((res) => {
         let data = res.data;
         this.newArrive = data.search_day_arrive_visitor_count;
         this.totalArrive = data.arrive_visitor_count;
@@ -109,8 +125,8 @@ export default {
       });
 
       this.date = `${this.formatDate(start)} ~ ${this.formatDate(end)}`;
-    }
-  }
+    },
+  },
 };
 </script>
 
@@ -123,32 +139,22 @@ export default {
   justify-content: center;
 }
 
-.custom_time_time {
-  overflow: hidden;
-  .custom_time_input {
-    font-size: 14px;
-    font-weight: 500;
-    text-align: center;
-    width: 100%;
-    color: #787878;
-  }
+/deep/ .van-cell__value {
+  text-align: center;
+  color: black;
 }
-.custom_time_content_header {
-  height: 35px;
-  line-height: 35px;
-  color: #00a862;
-  font-size: 14px;
-  display: flex;
-  justify-content: space-around;
-  background: rgba(0, 168, 98, 0.08);
+.van-collapse-item__content {
+  padding: 5px;
 }
-.custom_time_content_body {
-  height: 45px;
-  line-height: 45px;
-  color: #787878;
-  font-size: 14px;
+
+.custom_content_body {
   display: flex;
-  justify-content: space-around;
+  justify-content: space-evenly;
+  padding: 5px;
+}
+
+span {
+  width: 33%;
 }
 span {
   width: 33%;
